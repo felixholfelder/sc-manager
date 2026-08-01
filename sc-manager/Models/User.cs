2a460c0e582305e1d2d92100ea8efd5ad1f3ffe4
Felix Holfelder <felixholfelder1@t-online.de>
2026-08-01 15:12:26 +0200

    display nickname of user in appbar

--- sc-manager/Models/User.cs
+++ sc-manager/Models/User.cs
@@ -6,5 +6,6 @@ public class User
     public string Id { get; set; } = "";
     public string Firstname { get; set; } = "";
     public string Lastname { get; set; } = "";
+    public string Nickname { get; set; } = "";
     public string Email { get; set; } = "";
 }
